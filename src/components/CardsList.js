a17d3994ab43a8fefc5b4ec0589c89e0637b53ff
ralph kemp <ralphk3mp@gmail.com>
2018-09-25 20:26:00 +0100

    ok created two components and added styled-components, time to get to work. Also deleted yarn

---
+++ src/components/CardsList.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styled from 'styled-components';
+
+class CardsList extends Component {
+  render() {
+    return <p>here are the cards</p>;
+  }
+}
+
+export default CardsList;
