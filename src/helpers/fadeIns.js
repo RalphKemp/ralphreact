3fa7b85550ebb1a0a0dcd7c0f12a91fe9d65a257
ralph kemp <ralphk3mp@gmail.com>
2018-09-26 02:33:53 +0100

    small css changes and bean

--- src/helpers/fadeIns.js
+++ src/helpers/fadeIns.js
@@ -2,10 +2,9 @@ import { TimelineMax, TweenMax } from "gsap";
 
 export function fadeCards() {
   const tl = new TimelineMax();
-  tl.to("#card-one", 0.4, { opacity: 1, delay: 0.3})
-    .to("#card-two", 0.4, { opacity: 1 });
+  tl.to("#card-one", 0.4, { opacity: 1, delay: 0.2 }).to("#card-two", 0.4, {
+    opacity: 1
+  });
 
-  TweenMax.to("#bean-div", 0.8, { opacity: 1, delay: 1.1 })
+  TweenMax.to("#bean-div", 0.7, { opacity: 1, delay: 1 });
 }
-
-//hey
