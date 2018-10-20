cbc55911e77a32e19e2ee5caa2a3fd0b83dbdee9
ralph kemp <ralphk3mp@gmail.com>
2018-10-20 20:37:08 +0100

    trying to enforce https, lets see if this works

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import CardsList from "./components/CardsList";
 import Header from "./components/Header";
+import HttpsRedirect from 'react-https-redirect';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <CardsList />
-      </div>
+      <HttpsRedirect>
+        <div className="App">
+          <Header />
+          <CardsList />
+        </div>
+      </HttpsRedirect>
     );
   }
 }
